8c7081a53d890fcc494f9088a7a344f13fa0c00d
bugmarcus <marcusq@hotmail.com>
2023-10-05 11:30:54 -0300

    V0.2

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -1,14 +1,8 @@
-import styles from "./Description.module.css";
-
-const Description = () => {
+export const Description = () => {
   return (
-    <div className={styles.component1}>
-      <div className={styles.label}>Descrição</div>
-      <div className={styles.helperText1}>Ex: Compra de roupas</div>
-      <div className={styles.placeholderWrapper}>
-        <div className={styles.placeholder}>Digite aqui sua descrição</div>
-      </div>
-    </div>
+    <form>
+      <input type="text" placeholder="Digite aqui sua descrição" />
+    </form>
   );
 };
 
